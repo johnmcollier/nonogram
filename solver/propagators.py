465640da2826e2ccbfeed11465fb53180801990c
David Sanders <david.sanders@mail.utoronto.ca>
2016-12-05 15:01:23 -0500

    implement forward checking

--- solver/propagators.py
+++ solver/propagators.py
@@ -26,3 +26,25 @@ def prop_BT(csp, newVar=None):
                 return False, []
     return True, []
 
+def prop_FC(csp, newVar=None):
+    pruned_vals = []
+    if not newVar:
+        return True, pruned_vals
+    for c in csp.get_cons_with_var(newVar):
+        if c.get_n_unasgn() == 1:
+            unassigned_vars = c.get_unasgn_vars()
+            unassigned_var = unassigned_vars[0]
+
+            for val in unassigned_var.cur_domain():
+                has_support = c.has_support(unassigned_var, val)
+                if(not has_support):
+                    pruned_vals.append((unassigned_var, val))
+
+        if c.get_n_unasgn() == 0:
+            vals = []
+            vars = c.get_scope()
+            for var in vars:
+                vals.append(var.get_assigned_value())
+            if not c.check(vals):
+                return False, []
+    return True, pruned_vals
